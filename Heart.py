16393c10a6e2c256acac383622d274a21a1f6185
Karina <rudenko344@gmail.com>
2025-10-18 02:07:42 +0200

    import data in py

--- Heart.py
+++ Heart.py
@@ -12,3 +12,22 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
+
+data = pd.read_csv("task_data.csv")
+# some data are string type, so we need to make them float type
+for col in data:
+    if type(col) == str:
+        data[col] = data[col].astype(str).str.replace(',', '.').astype(float)
+
+X = data[[
+   'Heart width', 'Lung width', 'CTR - Cardiothoracic Ratio', 'xx', 'yy', 'xy', 'normalized_diff', 'Inscribed circle radius', 'Polygon Area Ratio', 'Heart perimeter', 'Heart area ', 'Lung area'
+]]                          # features
+y = data["Cardiomegaly"]    #target column
+
+# training 80%, testing 20%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler();
+X_scaled_train = scaler.fit_transform(X_train)
+X_scaled_test = scaler.transform(X_test)
+
